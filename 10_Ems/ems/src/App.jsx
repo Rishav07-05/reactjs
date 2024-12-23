738da24f6535d41df7009c8fade419059a8988d2
Rishav Kumar <rishavkumar18902@gmail.com>
2024-12-23 20:16:25 +0530

    Worked finished in login.jsx

--- 10_Ems/ems/src/App.jsx
+++ 10_Ems/ems/src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Login from './components/Auth/login'
 
 const App = () => {
   return (
-    <div className='text-2xl font-bold text-blue-600' >App</div>
+    <>
+      <Login/>
+    </>
   )
 }
 
---
+++ 10_Ems/ems/src/components/Auth/login.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+const Login = () => {
+
+    const [email , setemail] = useState('')
+    const [password , setpassword] = useState('')
+    
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        console.log("Email is ", email, " Password is ", password);
+        
+        setemail("")
+        setpassword("");
+    }
+
+
+    return (
+        <div className='h-screen w-screen flex items-center justify-center'>
+            <div className='border-2 rounded-2xl border-emerald-500 p-20'>
+                <form
+                    onSubmit={(e) => {
+                            submitHandler(e)
+                    }}
+                    className='flex flex-col items-center justify-center'>
+                    <input
+                        value={email}
+                        onChange={(e) => {
+                            setemail(e.target.value)
+                        }}
+                        required className='border-2 border-emerald-600 rounded-full py-3 px-5 text-xl  outline-none bg-transparent placeholder:text-gray-600' type="email" placeholder='Enter your email' />
+                    <input
+                        value={password}
+                        onChange={(e) => {
+                            setpassword(e.target.value)
+                        }}
+                        required className='border-2 border-emerald-600 rounded-full py-3 px-5 text-xl  outline-none bg-transparent placeholder:text-gray-600 mt-5' type="password" placeholder='Password' />
+                    <button className='mt-5 border-none bg-emerald-600 rounded-full py-3 px-5 text-xl text-White outline-none' type="submit">Log in</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Login
