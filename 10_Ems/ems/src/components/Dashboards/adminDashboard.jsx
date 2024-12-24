8a0010ae6bd760ded46e6e6a1226720fce6037da
Rishav Kumar <rishavkumar18902@gmail.com>
2024-12-24 14:02:03 +0530

    added alltask.jsx

--- 10_Ems/ems/src/components/Dashboards/adminDashboard.jsx
+++ 10_Ems/ems/src/components/Dashboards/adminDashboard.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../../others/header';
 import CreateTask from '../../others/createTask';
+import AllTask from '../../others/allTask';
 
 const AdminDashboard = () => {
   return (
     <div className='h-screen w-full p-6 '>
       <Header />
-      <CreateTask/>
+        <CreateTask />
+        <AllTask/>
     </div>
   );
 };
---
+++ 10_Ems/ems/src/others/allTask.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+function AllTask() {
+    return (
+        <div id='alltask' className='bg-[#242222] p-5 rounded-lg mt-20 h-60 overflow-auto'>
+            <div className='mb-6 bg-red-500 py-2 px-4 flex justify-between rounded-full '>
+                <h2>Sarthak</h2>
+                <h3>Make a UI Design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='mb-6 bg-blue-500 py-2 px-4 flex justify-between rounded-full'>
+                <h2>Sarthak</h2>
+                <h3>Make a UI Design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='mb-6 bg-green-500 py-2 px-4 flex justify-between rounded-full'>
+                <h2>Sarthak</h2>
+                <h3>Make a UI Design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='mb-6 bg-yellow-500 py-2 px-4 flex justify-between rounded-full'>
+                <h2>Sarthak</h2>
+                <h3>Make a UI Design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='mb-6 bg-pink-500 py-2 px-4 flex justify-between rounded-full'>
+                <h2>Sarthak</h2>
+                <h3>Make a UI Design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='mb-6 bg-purple-500 py-2 px-4 flex justify-between rounded-full'>
+                <h2>Sarthak</h2>
+                <h3>Make a UI Design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='mb-6 bg-emerald-500 py-2 px-4 flex justify-between rounded-full'>
+                <h2>Sarthak</h2>
+                <h3>Make a UI Design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='mb-6 bg-red-500 py-2 px-4 flex justify-between rounded-full'>
+                <h2>Sarthak</h2>
+                <h3>Make a UI Design</h3>
+                <h5>Status</h5>
+            </div>
+            <div className='mb-6 bg-blue-500 py-2 px-4 flex justify-between rounded-full'>
+                <h2>Sarthak</h2>
+                <h3>Make a UI Design</h3>
+                <h5>Status</h5>
+            </div>
+        </div>
+    )
+}
+
+export default AllTask
