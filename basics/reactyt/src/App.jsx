c5165ccb4e9f9e3ad6502fb9ff404750a6fde7d5
Rishav Kumar <rishavkumar18902@gmail.com>
2024-12-26 15:32:55 +0530

    Revising react hooks

---
+++ basics/reactyt/src/App.jsx
@@ -0,0 +1,36 @@
+// import React from "react"
+import { useState } from "react"
+
+const App = () => {
+
+  const [user, setUser] = useState('Riss');
+
+  const changeUser = () => {
+    setUser("Admin")
+  }
+
+  const PrevUser = () => {
+    setUser('Risss')
+  }
+
+  const [num , setNum] = useState(0)
+
+  return (
+    <div>
+      <h1>My name is {user}</h1>
+      <button onClick={changeUser} >Change User</button>
+      <button onClick={PrevUser} >Previous User</button>
+      <br />
+      <h1>My number is {num}</h1>
+      <button onClick={() => setNum(num + 1)} >Increment</button>
+      <button onClick={() => {
+        if (num > 0) {
+          setNum(num - 1)
+        }
+      }} >Decrement</button>
+    </div>
+  )
+}
+
+
+export default App
